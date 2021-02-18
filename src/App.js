c9659621e8b84c59f332f7a24d08514f04b95440
Alexsandr Chigarev <majestik13@mail.ru>
2021-02-18 16:06:45 +0300

    inicial

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi and save to reload.
+          Hi! and save to reload.
         </p>
         <a
           className="App-link"
