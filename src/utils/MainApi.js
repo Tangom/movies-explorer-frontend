6eb5e6247cf0efc3c2d75db7fb24ef13ff9a286d
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-20 10:49:21 +0300

    cors

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -126,6 +126,7 @@ class MainApi {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': '*',
       'Access-Control-Allow-Headers': '*',
+      'Access-Control-Allow-Credentials': '*',
     },
   })
   
