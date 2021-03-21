1bff426327de2ad4c43c0ae486b990d45058401d
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-21 21:42:03 +0300

    level-3

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -3,28 +3,28 @@ import {useLocation} from "react-router";
 
 function MoviesCard({onSaveMovie, moviesCard, onBookmarkClick}) {
   const location = useLocation();
-  const {duration, image, trailer, nameRU} = moviesCard;
+  // const {duration, image, trailer, nameRU} = moviesCard;
 
   let isSave = onSaveMovie(moviesCard);
 
   function handleClick(evt) {
     evt.preventDefault();
-    onBookmarkClick(moviesCard, !isSave)
+    onBookmarkClick(moviesCard, isSave)
   }
 
   return (
     <div className="movies-card">
       <div className="movies-card__content">
         <div className="movies-card__description">
-          <h2 className="movies-card__title">{nameRU}</h2>
-          <p className="movies-card__duration">{duration}</p>
+          <h2 className="movies-card__title">{moviesCard.nameRU}</h2>
+          <p className="movies-card__duration">{moviesCard.duration}</p>
         </div>
         <button
           className={`movies-card__button ${(location.pathname === "/saved-movies" ? "movies-card__button_delete" : "")}`}
           type="button" onClick={handleClick}/>
       </div>
-      <a href={trailer} target="_blank">
-        <img className="movies-card__image" alt={nameRU} src={image}/>
+      <a href={moviesCard.trailer} target="_blank">
+        <img className="movies-card__image" alt={moviesCard.nameRU} src={moviesCard.image}/>
       </a>
     </div>
   )
