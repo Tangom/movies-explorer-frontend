9764742402ee1b3520dcc895572c89d98d0d3030
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-22 18:31:54 +0300

    level-3

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -15,19 +15,27 @@ function Profile(saveProfile, signOut) {
 
   const [isValid, setIsValid] = React.useState(true);
 
+
   const currentUser = React.useContext(CurrentUserContext);
 
-  React.useEffect(() => {
-    setInputValue({
-      ...inputValue,
-      name: currentUser.name || '',
-      email: currentUser.email || ''
-    })
-  }, [currentUser]);
+  // React.useEffect(() => {
+  //   setInputValue({
+  //     ...inputValue,
+  //     name: currentUser.name || '',
+  //     email: currentUser.email || ''
+  //   })
+  // }, [currentUser]);
 
-  function handleSignOut() {
-    signOut();
-  }
+  React.useEffect(() => {
+    if (
+      !inputError.name &&
+      !inputError.email
+    ) {
+      setIsValid(false)
+    } else {
+      setIsValid(true)
+    }
+  }, [inputError]);
 
   function handleOnSubmit(evt) {
     evt.preventDefault();
@@ -61,7 +69,8 @@ function Profile(saveProfile, signOut) {
       <h1 className="profile__title">Привет, {currentUser.name}!</h1>
       <form className="profile__form" action="post" name="profile" noValidate onSubmit={handleOnSubmit}>
         <label className="profile__label">Имя
-          <input className="profile__input profile__input_error" type="text" minLength="2" maxLength="30" required
+          <input className="profile__input" type="text" minLength="2" maxLength="30" required
+                 style={{color: inputError.name ? 'red' : 'white'}}
                  value={inputValue.name} onChange={(evt) => {
             handleName(evt)
           }}/>
@@ -69,6 +78,7 @@ function Profile(saveProfile, signOut) {
         <div className="profile__line"/>
         <label className="profile__label">Почта
           <input className="profile__input" type="Email" minLength="6" maxLength="40" required value={inputValue.email}
+                 style={{color: inputError.name ? 'red' : 'white'}}
                  onChange={(evt) => {
                    handleEmail(evt)
                  }}/>
@@ -81,7 +91,7 @@ function Profile(saveProfile, signOut) {
                   disabled={isValid}>>
             Редактировать
           </button>
-          <button className="profile__button profile__button_color" onClick={handleSignOut}>
+          <button className="profile__button profile__button_color" onClick={signOut}>
             Выйти из аккаунта
           </button>
         </div>
