13c4c69d40a9c8b7ea24652cbf65da3f04a892b9
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-25 17:01:33 +0300

    oh

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -4,6 +4,77 @@ class MainApi {
     this._headers = data.headers;
   }
 
+  _showErrow(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    // если ошибка, отклоняем промис
+    Promise.reject(new Error(`Ошибка: ${res.status}`));
+  }
+
+  authorize(data) {
+    return fetch(`${this._url}/signin`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    })
+      .then((res) => { return res.json() })
+      .then((data) => {
+        if (data.token) {
+          // сохранение токена в localStorage
+          localStorage.setItem('token', data.token);
+          return data;
+        } else {
+          return;
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // запрос на поллучение токена
+  getToken() {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      headers: this._headers,
+    })
+      .then((res) => { return res.json() })
+      .then(data => data)
+      .catch((err) => console.log(err));
+  }
+
+  // запрос пользователя
+  getUserInfo() {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      headers: this._headers,
+    }).then((res) => this._showErrow(res));
+  }
+
+  // запрос пользователей
+  getUsers() {
+    return fetch(`${this._url}/users`, {
+      method: 'GET',
+      headers: this._headers,
+    })
+      .then((res) => { return res.json() })
+      .then(data => data)
+      .catch((err) => console.log(err));
+  }
+
+  // изменение данных пользователя
+  setUserInfo(data) {
+    return fetch(`${this._url}/users/me`, {
+      method: 'PATCH',
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then((res) => this._showErrow(res));
+  }
+
+
+
+
+
+
   _getResponseData(response) {
     return response.then((res) => {
       if (res.ok) {
@@ -135,6 +206,7 @@ const mainApi = new MainApi({
   url: 'https://api.tango.students.nomoredomains.icu',
   headers: {
     'Content-Type': 'application/json',
+    authorization: `Bearer ${localStorage.getItem('token')}`
   },
   credentials: 'include',
 })
