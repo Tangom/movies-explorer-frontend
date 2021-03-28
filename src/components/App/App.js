554b81bc9efacfafd45fb05ebc25e6bc32ff498d
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-28 21:18:37 +0300

    fignya

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -61,61 +61,61 @@ function App() {
     })
   }
 
-  // function handlerLogin() {
-  //   const token = localStorage.getItem('token');
-  //   if (token !== null) {
-  //     mainApi.getToken(token)
-  //       .then((data) => {
-  //         if (data) {
-  //           setCurrentUser(data);
-  //           setLoggedIn(true);
-  //           history.push('/movies');
-  //         }
-  //       }).catch((err) => {
-  //       console.log(err);
-  //       signOut();
-  //     })
-  //   } else signOut();
-  // }
+  function handlerLogin() {
+    const token = localStorage.getItem('token');
+    if (token !== null) {
+      mainApi.getToken(token)
+        .then((data) => {
+          if (data) {
+            setCurrentUser(data);
+            setLoggedIn(true);
+            history.push('/movies');
+          }
+        }).catch((err) => {
+        console.log(err);
+        signOut();
+      })
+    } else signOut();
+  }
 
   // сохранение токена для повторного входа
-  // React.useEffect(() => {
-  //   handlerLogin();
-  // }, [loggedIn]);
-
-
-  // function submitLogin(data) {
-  //   mainApi.login(data).then((data) => {
-  //     if (data) {
-  //       handlerLogin();
-  //       setCurrentUser(data);
-  //       history.push('/');
-  //     }
-  //   })
-  //     .catch(err => {
-  //       console.log(err);
-  //     })
-  // }
-
   React.useEffect(() => {
-    const path = location.pathname;
-    const token = localStorage.getItem('token');
-    if (token) {
-      mainApi.checkToken(token)
-        .then((res) => {
-          if (res) {
-            setLoggedIn(true);
-            getCurrentUser();
-            history.push(path);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          localStorage.removeItem('token')
-          history.push('/');
-        });
-    }
-  }, []);
+    handlerLogin();
+  }, [loggedIn]);
+
+
+  function submitLogin(data) {
+    mainApi.login(data).then((data) => {
+      if (data) {
+        handlerLogin();
+        setCurrentUser(data);
+        history.push('/');
+      }
+    })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  // React.useEffect(() => {
+  //   const path = location.pathname;
+  //   const token = localStorage.getItem('token');
+  //   if (token) {
+  //     mainApi.checkToken(token)
+  //       .then((res) => {
+  //         if (res) {
+  //           setLoggedIn(true);
+  //           getCurrentUser();
+  //           history.push(path);
+  //         }
+  //       })
+  //       .catch((err) => {
+  //         console.log(err);
+  //         localStorage.removeItem('token')
+  //         history.push('/');
+  //       });
+  //   }
+  // }, []);
 
   function onRegister(data) {
     mainApi.register(data).then((data) => {
@@ -128,41 +128,41 @@ function App() {
       });
   }
 
-  function login(email, password) {
-    mainApi.login(email, password)
-      .then((res) => {
-        if (res.token) {
-          localStorage.setItem('token', res.token);
-          setLoggedIn(true);
-          getCurrentUser();
-          history.push('/movies');
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  }
+  // function login(email, password) {
+  //   mainApi.login(email, password)
+  //     .then((res) => {
+  //       if (res.token) {
+  //         localStorage.setItem('token', res.token);
+  //         setLoggedIn(true);
+  //         getCurrentUser();
+  //         history.push('/movies');
+  //       }
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     })
+  // }
 
-  function submitLogin({email, password}) {
-    if (!email || !password) {
-      return;
-    }
-    login(email, password);
-  }
+  // function submitLogin({email, password}) {
+  //   if (!email || !password) {
+  //     return;
+  //   }
+  //   login(email, password);
+  // }
 
-  function getCurrentUser() {
-    const token = localStorage.getItem('token');
-    mainApi.getCurrentUser(token)
-      .then((res) => {
-        if (res) {
-          setCurrentUser(res)
-          localStorage.setItem('currentUser', JSON.stringify(res))
-        }
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  }
+  // function getCurrentUser() {
+  //   const token = localStorage.getItem('token');
+  //   mainApi.getCurrentUser(token)
+  //     .then((res) => {
+  //       if (res) {
+  //         setCurrentUser(res)
+  //         localStorage.setItem('currentUser', JSON.stringify(res))
+  //       }
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //     });
+  // }
   //
   // function saveProfile(data) {
   //   mainApi.saveProfile(data)
--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,9 +1,8 @@
 class MainApi {
-  constructor(data) {
-    this._url = data.url;
-    this._headers = data.headers;
+  constructor(options) {
+    this._url = options.url;
+    this._headers = options.headers;
   }
-
 
   _showErrow(res) {
     if (res.ok) {
@@ -13,193 +12,104 @@ class MainApi {
     Promise.reject(new Error(`Ошибка: ${res.status}`));
   }
 
-  getUserInfo() {
-    return fetch(`${this._url}/users/me`, {
+  getSaveMovies() {
+    return fetch(`${this._url}/movies`, {
       method: 'GET',
-      headers:{
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
+      headers: this._headers,
     }).then((res) => this._showErrow(res));
   }
 
-  setUserInfo(data) {
-    return fetch(`${this._url}/users/me`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
+  saveMovie(data) {
+    return fetch(`${this._url}/movies`, {
+      method: 'POST',
+      headers: this._headers,
       body: JSON.stringify(data),
     }).then((res) => this._showErrow(res));
   }
 
+  deleteMovie(id) {
+    return fetch(`${this._url}/movies/${id}`, {
+      method: 'DELETE',
+      headers: this._headers,
+    }).then((res) => this._showErrow(res));
+  }
+
+  // запрос на регистрацию пользователя
+  register(data) {
+    return fetch(`${this._url}/signup`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    }).then((res) => this._showErrow(res));
+  };
+
+  // запрос на авторизацию пользователя
+  authorize(data) {
+    return fetch(`${this._url}/signin`, {
+      method: 'POST',
+      headers: this._headers,
+      body: JSON.stringify(data)
+    })
+      .then((res) => { return res.json() })
+      .then((data) => {
+        if (data.token) {
+          // сохранение токена в localStorage
+          localStorage.setItem('token', data.token);
+          return data;
+        } else {
+          return;
+        }
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // запрос на поллучение токена
   getToken() {
     return fetch(`${this._url}/users/me`, {
       method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        authorization: `Bearer ${localStorage.getItem('token')}`,
-      },
+      headers: this._headers,
     })
       .then((res) => { return res.json() })
       .then(data => data)
       .catch((err) => console.log(err));
   }
 
-  // login(data) {
-  //   return fetch(`${this._url}/signin`, {
-  //     method: 'POST',
-  //     headers: {
-  //       'Content-Type': 'application/json',
-  //       authorization: `Bearer ${localStorage.getItem('token')}`,
-  //     },
-  //     body: JSON.stringify(data)
-  //   })
-  //     .then((res) => { return res.json() })
-  //     .then((data) => {
-  //       if (data.token) {
-  //         // сохранение токена в localStorage
-  //         localStorage.setItem('token', data.token);
-  //         return data;
-  //       } else {
-  //         return;
-  //       }
-  //     })
-  //     .catch((err) => console.log(err));
-  // };
+  // запрос пользователя
+  getUserInfo() {
+    return fetch(`${this._url}/users/me`, {
+      method: 'GET',
+      headers: this._headers,
+    }).then((res) => this._showErrow(res));
+  }
 
-  _getResponseData(response) {
-    return response.then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      if (res.status === 409 || res.status === 404 || res.status === 400) {
-        return Promise.reject({
-          status: res.status
-        })
-      }
-      return Promise.reject(new Error(`Ошибка: ${res.status}`));
+  // запрос пользователей
+  getUsers() {
+    return fetch(`${this._url}/users`, {
+      method: 'GET',
+      headers: this._headers,
     })
+      .then((res) => { return res.json() })
+      .then(data => data)
+      .catch((err) => console.log(err));
   }
 
-  register(data) {
-    return this._getResponseData(fetch(`${this._url}/signup`, {
-      method: 'POST',
-      body: JSON.stringify({
-        "name": data.name,
-        "email": data.email,
-        "password": data.password
-      }),
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-    }))
-  }
-
-  login(email, password) {
-    return this._getResponseData(fetch(`${this._url}/signin`, {
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        "email": email,
-        "password": password
-      })
-    }))
-  }
-
-  checkToken(token) {
-    return this._getResponseData(fetch(`${this._url}/users/me`, {
-      method: 'GET',
-      headers: {
-        ...this._headers,
-        "Authorization": `Bearer ${token}`
-      }
-    }))
-  }
-
-  getCurrentUser(token) {
-    return this._getResponseData(fetch(`${this._url}/users/me`, {
-      method: 'GET',
-      headers: {
-        ...this._headers,
-        "Authorization": `Bearer ${token}`
-      }
-    }))
-  }
-
-  saveProfile(data) {
-    const token = localStorage.getItem('token');
-    return this._getResponseData(fetch(`${this._url}/users/me`, {
+  // изменение данных пользователя
+  setUserInfo(data) {
+    return fetch(`${this._url}/users/me`, {
       method: 'PATCH',
-      headers: {
-        ...this._headers,
-        "Authorization": `Bearer ${token}`
-      },
-      body: JSON.stringify({
-        name: data.name,
-        email: data.email
-      })
-    }))
-  }
-
-  getMovies() {
-    const token = localStorage.getItem('token');
-    return this._getResponseData(fetch(`${this._url}/movies`, {
-      method: 'GET',
-      headers: {
-        ...this._headers,
-        "Authorization": `Bearer ${token}`
-      }
-    }))
-  }
-
-  createMovie(data) {
-    const token = localStorage.getItem('token');
-    return this._getResponseData(fetch(`${this._url}/movies`, {
-      method: 'POST',
-      headers: {
-        ...this._headers,
-        "Authorization": `Bearer ${token}`
-      },
-      body: JSON.stringify({
-        country: data.country,
-        director: data.director,
-        duration: data.duration,
-        year: data.year,
-        description: data.description,
-        image: data.image,
-        trailer: data.trailer,
-        thumbnail: data.image,
-        movieId: data.id,
-        nameRU: data.nameRU,
-        nameEN: data.nameEN,
-      })
-    }))
-  }
-
-  deleteMovies(movieId) {
-    const token = localStorage.getItem('token');
-    return this._getResponseData(fetch(`${this._url}/movies/${movieId}`, {
-      method: 'DELETE',
-      headers: {
-        ...this._headers,
-        "Authorization": `Bearer ${token}`
-      }
-    }))
+      headers: this._headers,
+      body: JSON.stringify(data),
+    }).then((res) => this._showErrow(res));
   }
 }
 
+//создание экземпляра класса
 const mainApi = new MainApi({
-  url: 'https://api.tango.students.nomoredomains.icu',
+  url: 'https://api.lutowa.movies.students.nomoredomains.monster',
   headers: {
     'Content-Type': 'application/json',
+    authorization: `Bearer ${localStorage.getItem('token')}`,
   },
-  credentials: 'include',
-})
+});
 
 export default mainApi;
