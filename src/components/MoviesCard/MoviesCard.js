2413859cb149c7d6f44b7afe8e503fade24fa042
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-29 22:51:35 +0300

    Yes

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -18,7 +18,7 @@ function MoviesCard({onSaveMovie, moviesCard, deleteMovieCard, onBookmarkClick})
   function duration(duration) {
     const hh = Math.trunc(duration / 60)
     const mm = duration % 60;
-    return `${hh>0 ? hh+'ч ' : ''}${mm}м`
+    return `${hh > 0 ? hh + 'ч ' : ''}${mm}м`
   }
 
   return (
@@ -29,8 +29,8 @@ function MoviesCard({onSaveMovie, moviesCard, deleteMovieCard, onBookmarkClick})
           <p className="movies-card__duration">{duration(moviesCard.duration)}</p>
         </div>
         <button
-          className={`movies-card__button ${isSave ? "movies-card__button_save" : ''}${(location.pathname === "/saved-movies" ? "movies-card__button_delete" : "")}`}
-          type="button" onClick={location.pathname === "/saved-movies" ?handleOnDelete:handleOnClick}/>
+          className={`movies-card__button ${isSave ? "movies-card__button_save" : ''} ${(location.pathname === "/saved-movies" ? "movies-card__button_delete" : "")}`}
+          type="button" onClick={location.pathname === "/saved-movies" ? handleOnDelete : handleOnClick}/>
       </div>
       <a href={moviesCard.trailer} target="_blank">
         <img className="movies-card__image" alt={moviesCard.nameRU} src={moviesCard.image}/>
