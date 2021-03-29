10f267f5953dab8277fa2238eb1a2b01c11280e9
Alexsandr Chigarev <majestik13@mail.ru>
2021-03-30 02:20:48 +0300

    Return

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,4 +1,4 @@
-import React from "react";
+import  React from "react";
 import SearchForm from "../SearchForm/SearchForm";
 import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Preloader from '../Preloader/Preloader';
@@ -7,6 +7,39 @@ function Movies({submitSearch, isLoading, deleteMovieCard, moviesCard, onSaveMov
 
   const [shortFilm, setShortFilm] = React.useState(false);
 
+  const [extraPortion, setExtraPortion] = React.useState(3);
+  const [currentCount, setCurrenCount] = React.useState(0);
+  const [renderMovies, setRenderMovies] = React.useState([]);
+
+  function getCount(windowSize) {
+    if (windowSize > 1190) {
+      return {first: 12, extra: 3}
+    } else if (windowSize > 480 && windowSize <= 767) {
+      return {first: 8, extra: 2}
+    } else {
+      return {first: 5, extra: 2}
+    }
+  }
+
+  function renderPortion() {
+    const count = Math.min(moviesCard.length, currentCount + extraPortion);
+    const extraMovies = moviesCard.slice(currentCount, count)
+    setRenderMovies([...renderMovies, ...extraMovies]);
+    setCurrenCount(count);
+  }
+
+  function handleResize() {
+    const windowSize = window.innerWidth;
+    const sizePortion = getCount(windowSize);
+    setExtraPortion(sizePortion.extra);
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
 
   function onFilter(filterOn) {
     setShortFilm(filterOn);
@@ -27,6 +60,7 @@ function Movies({submitSearch, isLoading, deleteMovieCard, moviesCard, onSaveMov
         onBookmarkClick={onBookmarkClick}
         onSaveMovie={onSaveMovie}
         moviesCard={shortFilm ? filterShortFilm(moviesCard) : moviesCard}/>
+      {moviesCard.length > 12 && <button className="movies__more" onClick={renderPortion}>Ещё</button>}
     </section>
   );
 }
--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,69 +1,21 @@
-import React from 'react';
 import MoviesCard from "../MoviesCard/MoviesCard";
 
 function MoviesCardList({moviesCard, onSaveMovie, deleteMovieCard, onBookmarkClick}) {
-
-  const [extraPortion, setExtraPortion] = React.useState(3);
-  const [currentCount, setCurrenCount] = React.useState(0);
-  const [renderMovies, setRenderMovies] = React.useState([]);
-
-  function getCount(windowSize) {
-    if (windowSize > 1190) {
-      return {first: 12, extra: 3}
-    } else if (windowSize > 480 && windowSize <= 767) {
-      return {first: 8, extra: 2}
-    } else {
-      return {first: 5, extra: 2}
-    }
-  }
-
-  function renderPortion() {
-    const count = Math.min(moviesCard.length, currentCount + extraPortion);
-    const extraMovies = moviesCard.slice(currentCount, count)
-    setRenderMovies([...renderMovies, ...extraMovies]);
-    setCurrenCount(count);
-  }
-
-  function handleResize() {
-    const windowSize = window.innerWidth;
-    const sizePortion = getCount(windowSize);
-    setExtraPortion(sizePortion.extra);
-  }
-
-  React.useEffect(() => {
-    window.addEventListener('resize', handleResize);
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  }, []);
-
-  React.useEffect(() => {
-    const windowSize = window.innerWidth;
-    const sizePortion = getCount(windowSize);
-    setExtraPortion(sizePortion.extra);
-    const count = Math.min(moviesCard.length, sizePortion.first);
-    setRenderMovies(moviesCard.slice(0, count));
-    setCurrenCount(count);
-  }, [moviesCard])
-
-
-  renderMovies.map((item) =>
-    (<li>
-      <MoviesCard
-        key={item.id}
-        onBookmarkClick={onBookmarkClick}
-        moviesCard={item}
-        onSaveMovie={onSaveMovie}
-        deleteMovieCard={deleteMovieCard}
-      />
-    </li>));
-  return ((moviesCard && moviesCard.length) || 0) > 0 ?
+  const cardElements = moviesCard.slice(0, 12)
+    .map((item) =>
+      (<li key={item.id}>
+        <MoviesCard
+          onBookmarkClick={onBookmarkClick}
+          moviesCard={item}
+          onSaveMovie={onSaveMovie}
+          deleteMovieCard={deleteMovieCard}
+        />
+      </li>));
+  return ((moviesCard && moviesCard.length) || 0)> 0 ?
     <ul className="cards">
-      {renderMovies}
+      {cardElements}
     </ul>
     : <p className="cards__not-found">Ничего не найдено</p>;
-
-  {moviesCard.length > 12 && <button className="movies__more" onClick={renderPortion}>Ещё</button>}
 }
 
 export default MoviesCardList;
